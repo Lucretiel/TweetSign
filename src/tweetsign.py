0a7682a762de7732cfca0a646f131caa13df147d
Nathan West <nathan@mimedia.com>
2013-02-07 15:37:37 -0500

    Rewrote to use dead_simpe

--- src/tweetsign.py
+++ src/tweetsign.py
@@ -10,38 +10,49 @@ import httplib
 
 import keys #THIS IS NOT IN GITHUB
 
+def validate_sign():
+    print 'Validating Sign'
+    connection = httplib.HTTPConnection('localhost', 39999)
+    connection.request('GET', '/dead-simple/isready')
+    result = json.loads(connection.getresponse().read())
+
+    if not result['ready']:
+        print 'Sing is not ready. Resetting...'
+        request = '/dead-simple/reset'
+        print request
+        connection.request('GET', request)
+        response = connection.getresponse()
+        if response.status >= 400
+            raise RuntimeError('Error resetting sign', response)
+        print response.read()
+
+    request = '/dead-simple/send?text={red}{a}: {green}{b}'
+    print request
+    connection.request('GET', request)
+    print connection.getresponse().read()
+    connection.close()
+
 class SignListener(StreamListener):
     def on_status(self, status):
         print "Got a tweet!"
-        text = status.text
-        if '#mimedia' not in text:
-            print "  This tweet did not contain #mimedia..."
-            print "  Here's the tweet %s" % text
-            return True
-        
-        message = {'text': '{red}{user}: {green}{tweet}',
-                   'name': 'tweet',
-                   'temporary': True,
-                   'fields': {'user': {'text': status.user.screen_name},
-                              'tweet': {'text': text}}}
-        print "Assembled message %s" % message
-        message = json.dumps(message)
-        connection = httplib.HTTPConnection('localhost', 39999)
-        headers = {'Content-Type': 'application/json'}
-        print "  Sending request..."
-        connection.request('POST', '/sign-controller/clumps/', message, headers)
-        result = connection.getresponse()
-        if result.status >= 400:
-            print "There was an error"
-            return True
-        clump_id = result.read()
-        print "Created message with ID %s" % clump_id
-        print "  Displaying..."
-        message = json.dumps({'ID': clump_id})
-        print "  Sending request..."
-        connection.request('PUT', '/sign-controller/active-clump', message, headers)
+
+        request_template = '/dead-simple/send?label=%s&text=%s'
+
+        labels = ('a', 'b')
+        data = (status.user.screen_name, status.text)
+
+        connection = HTTPConnection('localhost', 39999)
+        for request_params in zip(labels, data):
+            request = request_template % request_params
+            print request
+            connection.request('GET', request_template % request_params)
+            result = connection.getresponse()
+            print result.read()
+            connection.close()
 
 if __name__ == '__main__':
+    validate_sign()
+
     listener = SignListener()
     auth = OAuthHandler(keys.consumer_key, keys.consumer_secret)
     auth.set_access_token(keys.access_token, keys.access_token_secret)
